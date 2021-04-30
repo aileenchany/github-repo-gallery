e9844a126721bb3cc96276ead01e54ebaf446762
Aileen CM <aileenchany@gmail.com>
2021-04-30 17:30:26 -0400

    Added functions that get specific repo info and displays it to the user

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const divElement = document.querySelector(".overview"); //selects <div class="overview">.This div is where your profile information will appear.
-const repoListElement = document.querySelector(".repo-list"); //selects the <ul> element
+const repoList = document.querySelector(".repo-list"); //selects the <ul> element
+const reposSection =document.querySelector(".repos"); //selects <section> element where all repo names will live in
+const repoDataSection = document.querySelector(".repo-data"); // selects <section> where each repo's data will be stored in 
 const username = "aileenchany";
 
 //Async function that fetches user data from GitHub API
@@ -34,7 +36,7 @@ const displayUserInfo = function(data) {
 const fetchMyRepos = async function() {
     const myReposUrl = await fetch(`https://api.github.com/users/${username}/repos`);
     const myReposData = await myReposUrl.json();
-    //console.log(myReposData);
+    console.log(myReposData);
     displayReposInfo(myReposData);
 };
 
@@ -44,8 +46,57 @@ const displayReposInfo = function(repos) {
         const li = document.createElement("li"); //created <li> 
         li.classList.add("repo"); //adding class to <li>
         li.innerHTML = `<h3>${repo.name}</h3>`; //adding repo name into <h3>
-        repoListElement.append(li); //appending aka 'saving' the <li> inside the <ul>
+        repoList.append(li); //appending aka 'saving' the <li> inside the <ul>
 
         //console.log(li);
     }
+};
+
+//Event listener for <ul> section
+repoList.addEventListener("click", function(e) {
+    //e.preventDefault();
+    if (e.target.matches("h3")) {
+        const repoName = e.target.innerText;
+
+        console.log(repoName);
+        getSpecificRepoInfo(repoName);
+       
+    }
+});
+
+//Function to Get Specific Repo Info
+const getSpecificRepoInfo = async function(name) {
+    const repoUrl = await fetch(`https://api.github.com/repos/${username}/${name}`); //end point parameters = /repos/{owner}/{repo}
+    const repoInfo = await repoUrl.json();
+
+    console.log(repoInfo);
+
+    //Fetch language data & create an Array of Languages
+    const fetchLanguages = await fetch(repoInfo.languages_url); //gets the languages_url data 
+    const languageData = await fetchLanguages.json();
+
+    console.log(languageData);
+
+    const languages = [];
+
+    for (const language in languageData) {
+        languages.push(language); //we push the language inside the data into the "languages" array
+    }
+    displaySpecificRepoInfo(repoInfo, languages);
+};
+
+//Function to Display Specific Repo Info
+const displaySpecificRepoInfo = function(repoInfo, languages) {
+    repoDataSection.innerHTML = "";
+    repoDataSection.classList.remove("hide");
+    reposSection.classList.add("hide");
+
+    const repoDataDiv = document.createElement("div");
+    repoDataDiv.innerHTML = `<h3>Name: ${repoInfo.name}</h3>
+    <p>Description: ${repoInfo.description}</p>
+    <p>Default Branch: ${repoInfo.default_branch}</p>
+    <p>Languages: ${languages.join(", ")}</p>
+    <a class="visit" href="${repoInfo.html_url}" target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>`;
+
+    repoDataSection.append(repoDataDiv);
 };
