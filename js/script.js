39a972597fc03069c6f079d656eea1c5a233a1e1
Aileen CM <aileenchany@gmail.com>
2021-04-30 18:53:10 -0400

    Added a return to repos button and added search box event

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const divElement = document.querySelector(".overview"); //selects <div class="ov
 const repoList = document.querySelector(".repo-list"); //selects the <ul> element
 const reposSection =document.querySelector(".repos"); //selects <section> element where all repo names will live in
 const repoDataSection = document.querySelector(".repo-data"); // selects <section> where each repo's data will be stored in 
+const backToRepoGallery = document.querySelector(".view-repos"); //selects the <button> that goes back to the repo gallery
+const filterInput = document.querySelector(".filter-repos"); //selects the <input> 
 const username = "aileenchany";
 
 //Async function that fetches user data from GitHub API
@@ -42,6 +44,8 @@ const fetchMyRepos = async function() {
 
 //Function that displays info about my repos
 const displayReposInfo = function(repos) {
+    filterInput.classList.remove("hide"); //here we display the search box
+
     for (const repo of repos) { 
         const li = document.createElement("li"); //created <li> 
         li.classList.add("repo"); //adding class to <li>
@@ -87,9 +91,11 @@ const getSpecificRepoInfo = async function(name) {
 
 //Function to Display Specific Repo Info
 const displaySpecificRepoInfo = function(repoInfo, languages) {
-    repoDataSection.innerHTML = "";
+    //Show or Hide elements
+    repoDataSection.innerHTML = ""; //single repo info
     repoDataSection.classList.remove("hide");
-    reposSection.classList.add("hide");
+    reposSection.classList.add("hide"); //all repos
+    backToRepoGallery.classList.remove("hide");
 
     const repoDataDiv = document.createElement("div");
     repoDataDiv.innerHTML = `<h3>Name: ${repoInfo.name}</h3>
@@ -100,3 +106,27 @@ const displaySpecificRepoInfo = function(repoInfo, languages) {
 
     repoDataSection.append(repoDataDiv);
 };
+
+//Click Event to the Back Button
+backToRepoGallery.addEventListener("click", function() {
+    repoDataSection.classList.add("hide");
+    reposSection.classList.remove("hide");
+    backToRepoGallery.classList.add("hide");
+});
+
+//Input Event to the Search Box
+filterInput.addEventListener("input", function(e) {
+    const input = e.target.value; //here we capture the input in a variable
+    //console.log(input);
+    const repos = document.querySelectorAll(".repo"); //here we search a class="repo" in all the elements
+    const lowercaseInput = input.toLowerCase(); //we lowercase the input
+    //Loop function that sorts thru the repos names. 
+    for (const repo of repos) { 
+        const repoLowercase = repo.innerText.toLowerCase(); //we lowercase the name of each repo
+        if (repoLowercase.includes(lowercaseInput)) { //if the repo name includes the typed input, we show the repos that contain the typed input
+            repo.classList.remove("hide"); 
+        } else {
+            repo.classList.add("hide");
+        }
+    };
+});
