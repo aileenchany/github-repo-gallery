7732bffe9713a3bbe09705dfb8cb6c7d3a554b75
Aileen CM <aileenchany@gmail.com>
2021-04-30 15:10:59 -0400

    Added functions to fetch my repos and display them

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 const divElement = document.querySelector(".overview"); //selects <div class="overview">.This div is where your profile information will appear.
+const repoListElement = document.querySelector(".repo-list"); //selects the <ul> element
 const username = "aileenchany";
 
 //Async function that fetches user data from GitHub API
 const getUsername = async function() {
     const apiUrl = await fetch(`https://api.github.com/users/${username}`);
     const data = await apiUrl.json();
-    console.log(data);
+    //console.log(data);
 
     displayUserInfo(data);
+    fetchMyRepos();
 };
 
 getUsername();
@@ -26,4 +28,24 @@ const displayUserInfo = function(data) {
       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div>`;
     divElement.append(userInfoElement); //we store "userInfoElement" inside "divElement"
+};
+
+//Function to fetch my repos
+const fetchMyRepos = async function() {
+    const myReposUrl = await fetch(`https://api.github.com/users/${username}/repos`);
+    const myReposData = await myReposUrl.json();
+    //console.log(myReposData);
+    displayReposInfo(myReposData);
+};
+
+//Function that displays info about my repos
+const displayReposInfo = function(repos) {
+    for (const repo of repos) { 
+        const li = document.createElement("li"); //created <li> 
+        li.classList.add("repo"); //adding class to <li>
+        li.innerHTML = `<h3>${repo.name}</h3>`; //adding repo name into <h3>
+        repoListElement.append(li); //appending aka 'saving' the <li> inside the <ul>
+
+        //console.log(li);
+    }
 };
